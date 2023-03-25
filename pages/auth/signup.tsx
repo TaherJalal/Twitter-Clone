348b321b5dc10f09e843e776786ebf05ed970135
unknown <taherjalaljuma@gmail.com>
2023-03-25 11:46:32 +0300

    added some styling for signup but no finish

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -21,37 +21,27 @@ function signup() {
 
 
   return (
-    <div className='w-60 capitalize'>
-        <form onSubmit={post}>
+    <>
+<div className='capitalize h-screen w-screen flex justify-center items-center font-montserrat'>
+    <form onSubmit={post} className='flex flex-col gap-6 w-80 bg-purple-300 rounded-md shadow-xl '>
 
-            <div className='flex flex-col'>
-                <label>first name</label>
-                <input type="text" onChange={(e) => setFirstName(e.target.value)}/>
-
-                <div className='flex flex-col'>
-                <label>last name</label>
-                <input type="text" onChange={(e) => setLastName(e.target.value)}/>
-            </div>
-
-            <div className='flex flex-col'>
-                <label>username</label>
-                <input type="text" onChange={(e) => setUserName(e.target.value)}/>
-            </div>
-
-            <div className='flex flex-col'>
-                <label>email address</label>
-                <input type="text" onChange={(e) => setEmail(e.target.value)}/>
-            </div>
-
-            <div className='flex flex-col'>
-                <label>password</label>
-                <input type="text" onChange={(e) => setPassword(e.target.value)}/>
-            </div>
-
-            </div>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
+        <input type="text" className='bg-purple-300 text-white placeholder-purple-400 focus:border-blue-400' required={true} placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
+             
+        <input type="text" className='bg-purple-300 text-white placeholder-purple-400' required={true} placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
+            
+           
+        <input type="text" className='bg-purple-300 text-white placeholder-purple-400' required={true} placeholder='User Name' onChange={(e) => setUserName(e.target.value)}/>
+            
+        <input type="email" className='bg-purple-300 text-white placeholder-purple-400' required={true} placeholder='Email Address' onChange={(e) => setEmail(e.target.value)}/>
+            
+        <input type="password" className='bg-purple-300 text-white placeholder-purple-400' required={true} placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
+      
+      <div className='flex justify-center'>
+    <button type="submit" className='hover:bg-purple-400 w-20 rounded-md'>Submit</button>
+      </div>
+    </form>
+</div>
+    </>
   )
 }
 
