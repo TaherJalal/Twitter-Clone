9b4913e45af3a576f15a09067313f5fce7b58ae3
t4h3r <taherjalaljuma@gmail.com>
2023-03-29 10:47:28 -0700

    myfollowing api

--- pages/api/follow/myfollowing.ts
+++ pages/api/follow/myfollowing.ts
@@ -1,3 +1,26 @@
-const myFollowings = await prisma.follow.findMany({
-  where: { followerId: userId },
-});
+import auth from "@/helper/auth";
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../lib/prisma";
+
+export default async function myFollowings(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    res.status(401).send("UnAuthorized");
+  }
+
+  if (req.method !== "POST") {
+    res.status(400).send("Not a POST Request");
+  }
+
+  const userId = await auth(token as string);
+
+  const myFollowings = await prisma.follow.findMany({
+    where: { followerId: userId },
+  });
+
+  res.json(myFollowings);
+}
