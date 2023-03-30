dd1d11a27beb828206e0aa0e8363a65e66d1686f
t4h3r <taherjalaljuma@gmail.com>
2023-03-30 08:21:35 -0700

    delete tweet api

---
+++ pages/api/tweet/delete.ts
@@ -0,0 +1,28 @@
+import auth from "@/helper/auth";
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prisma";
+
+export default async function deleteTweet(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    res.status(401).send("UnAuthorized");
+  }
+
+  if (req.method !== "POST") {
+    res.status(401).send("Not a POST Request");
+  }
+
+  const userId = await auth(token as string);
+
+  const { tweetId } = req.body;
+
+  const deleteTweet = await prisma.tweet.delete({
+    where: {
+      id: tweetId,
+    },
+  });
+}
--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,31 +1,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import {prisma} from '../../../lib/prisma'
+import { prisma } from "../../../lib/prisma";
 import auth from "@/helper/auth";
 
-export default async function userIndex(req: NextApiRequest , res: NextApiResponse){
-    const token = req.headers.authorization?.split(" ")[1];
+export default async function userIndex(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const token = req.headers.authorization?.split(" ")[1];
 
-    if(!token){
-        res.status(401).send("UnAuthorized")
-    }
+  if (!token) {
+    res.status(401).send("UnAuthorized");
+  }
 
-    if(req.method !== "GET"){
-        res.status(401).send("Not a GET Request")
-    }
+  if (req.method !== "GET") {
+    res.status(401).send("Not a GET Request");
+  }
 
-    const userId = await auth(token)
+  const userId = await auth(token as string);
 
+  const userDeatils = await prisma.user.findUniqueOrThrow({
+    where: {
+      id: userId,
+    },
+    include: {
+      followers: true,
+      following: true,
+    },
+  });
 
-    const userDeatils = await prisma.user.findUniqueOrThrow({
-        where:{
-            id: userId 
-        },
-        include:{
-            followers: true,
-            following: true
-        }
-    }) 
-
-    res.json(userDeatils)
-
+  res.json(userDeatils);
 }
